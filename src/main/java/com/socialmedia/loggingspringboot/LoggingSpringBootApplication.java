110c9493233d787055783e2a6ba4b8446e9c395e
Ali <aamirali15214@gmail.com>
2025-03-07 16:44:15 +0500

    init commit

---
+++ src/main/java/com/socialmedia/loggingspringboot/LoggingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.socialmedia.loggingspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggingSpringBootApplication.class, args);
+    }
+
+}
